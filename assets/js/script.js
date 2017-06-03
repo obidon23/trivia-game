a94220bc896067b3e3e5f351036b951a6444084d
Don Guckenberger <dguck23@gmail.com>
2017-06-03 15:53:30 -0600

    converted object to array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // VARIABLES==============================
 
-var ledZeppelin = {
+var ledZeppelin = [
 	
-	"question1": {
+	 {
 		question: "Who played drums for Led Zeppelin?",
 		answer1: "Jimmy Page",
 		answer2: "Robert Plant",
@@ -11,7 +11,7 @@ var ledZeppelin = {
 		correctAnswer: "John Bonham",
 	},
 
-	"question2": {
+	{
 		question: "What is the first song on Led Zeppelin 3?",
 		answer1: "Kashmir",
 		answer2: "Immigrant Song",
@@ -19,7 +19,7 @@ var ledZeppelin = {
 		answer4: "Thank You",
 		correctAnswer: "Immigrant Song",
 	},
-	"question3": {
+	 {
 		question: "What song charted highest for Led Zeppelin?",
 		answer1: "Stairway to Heaven",
 		answer2: "Whole Lotta Love",
@@ -28,7 +28,7 @@ var ledZeppelin = {
 		correctAnswer: "Whole Lotta Love",
 	},
 
-	"question4": {
+	 {
 		question: "Who was the youngest member of Led Zeppelin",
 		answer1: "Jimmy Page",
 		answer2: "Robert Plant",
@@ -37,47 +37,84 @@ var ledZeppelin = {
 		correctAnswer: "Robert Plant",
 	},
 
-	"question5": {
-		question: "In what year did Led Zeppelin's debut album get released?",
+	{
+				question: "In what year did Led Zeppelin's debut album get released?",
 		answer1: "1967",
 		answer2: "1968",
 		answer3: "1969",
 		answer4: "1970",
-		correctAnswer: "1971",
+		correctAnswer: "1969",
 	},
-}
-	var currentQuestionPage = ledZeppelin.question1;
-	var currentQuestion = ledZeppelin.question1.question;
-	var currentAnswer1  = ledZeppelin.question1.answer1;
-	var currentAnswer2 = ledZeppelin.question1.answer2;
-	var currentAnswer3 = ledZeppelin.question1.answer3;
-	var currentAnswer4 = ledZeppelin.question1.answer4;
-	var currentCorrectAnswer = ledZeppelin.question1.correctAnswer;
+]
+	var currentQuestionPage = ledZeppelin[0];
+	var currentQuestion = 0
+	var currentAnswer1  = ledZeppelin[0].answer1;
+	var currentAnswer2 = ledZeppelin[0].answer2;
+	var currentAnswer3 = ledZeppelin[0].answer3;
+	var currentAnswer4 = ledZeppelin[0].answer4;
+	var currentCorrectAnswer = ledZeppelin[0].correctAnswer;
 	var correctAnswers = 0;
 	var incorrectAnswers = 0;
 	var questionCount = 0;
+	var counter = 5;
 
-var counter = 20;
-var interval = setInterval(function() {
-    counter--;
-    // Display 'counter' wherever you want to display it.
-    if (counter == 0) {
-        // Display a login box
-        $("#countdown").html("<p>Time's Up!</p>")
-        clearInterval(interval);
-    }
 
-    else {	$("#countdown").html("<p>Seconds Remaining: " + counter + "</p>");
-}
-}, 1000);
 
 //FUNCTIONS====================================
+// function timer() {
+// 	var interval = setInterval(function() {
+//     	counter--;
+//     	// Display 'counter' wherever you want to display it.
+// 	    if (counter === 0) {
+//         // Display a login box
+//     	    $("#countdown").html("<p>Time's Up!</p>")
+//         	clearInterval(interval);
+//     	}
+
+// 	    else {	$("#countdown").html("<p>Seconds Remaining: " + counter + "</p>");
+// 		}
+// 	}, 1000);
+
+// 	nextQuestion();
+// }
 
 function nextQuestion() {
 
-	currentQuestionPage = ledZeppelin.key++;
-	$("#question").html(currentQuestion).append("<br/><input type='radio' value='" + currentAnswer1 +"'>" + currentAnswer1).append("<br/><input type='radio' value='" + currentAnswer2 +"'>" + currentAnswer2).append("<br/><input type='radio' value='" + currentAnswer3 +"'>" + currentAnswer3).append("<br/><input type='radio' value='" + currentAnswer4 +"'>" + currentAnswer4);
+	var interval = setInterval(function() {
+			
+    	counter--;
+    	// Display 'counter' wherever you want to display it.
+	    if (counter === 0) {
+        // Display a login box
+    	    $("#countdown").html("<p>Time's Up!</p>");
+        	clearInterval(interval);
+    		console.log("next question");
+    		counter = 5;
+        	// nextQuestion();
+    	}
+
+	    else {	$("#countdown").html("<p>Seconds Remaining: " + counter + "</p>");
+		}
+	}, 1000);	
+
+	// currentQuestion = key.question;
+	$("#question").html("<label>" + ledZeppelin[currentQuestion]+ "</label>")
+		.append("<br/><input type='radio' name = " +currentQuestion +"  value='" + currentAnswer1 +"'>" + currentAnswer1 + "<checked>")
+		.append("<br/><input type='radio' name = " + currentQuestion + "value='" + currentAnswer2 +"'>" + currentAnswer2 + "</>")
+		.append("<br/><input type='radio' name = " +currentQuestion +"  value='" + currentAnswer3 +"'>" + currentAnswer3 + "<checked>")
+		.append("<br/><input type='radio' name = " + currentQuestion + "value='" + currentAnswer4 +"'>" + currentAnswer4 + "</>");
 }
+
+
+$(function() {
+	$('#submitAnswer').submit(function() {
+		playerGuess = $('input(value)').val();
+		console.log("Player guessed " + playerGuess);
+		return false;
+	})
+});
+
+
 $("#submitAnswer").on("submit", function(e) {
 
 	if (this.value === correctAnswer) {
@@ -99,4 +136,7 @@ $("#submitAnswer").on("submit", function(e) {
 //DOM==========================================
 $("#countdown").html("<p>Seconds Remaining: " + counter + " </p>");
 nextQuestion();
+// timer();
+
 console.log(currentQuestionPage);
+console.log(currentCorrectAnswer);
